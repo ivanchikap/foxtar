6a316edd3df9eb6ceb8f04294709cf04fa53a336
ivanchikap <anton.ivanchik@gmail.com>
2021-11-29 15:09:45 +0200

    during hw-13

---
+++ src/js/init.js
@@ -0,0 +1,24 @@
+// $(function() {
+//     $("#services-tabs").tabs();
+//
+//     $("#portfolio__tabs").tabs();
+//
+//     $('#owl-carousel').owlCarousel({
+//         loop:true,
+//         items:1,
+//         nav: true,
+//         dots: true
+//     });
+// });
+
+
+
+let menuBtn = document.getElementById('nav__btn');
+
+
+menuBtn.addEventListener("click", function() {
+    let menu = document.getElementById('nav__nav');
+    setTimeout(function () {
+        menu.classList.toggle('d-sm-none');
+    }, 100)
+});
